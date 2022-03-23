26c300528be4dac1dcd0b3d74ada5cfabf37c195
Mathis Boston <87693719+Tiss971@users.noreply.github.com>
2022-03-23 16:23:18 +0100

    implement dark/light mode

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import i18next from '../i18n'
+import i18next from '../i18n';
 
+import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -14,45 +15,119 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Select from '@mui/material/Select';
-
-import { useTheme } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Switch from '@mui/material/Switch';
 
 const pages = ['Latest', 'Informations', 'Stats', 'Chat'];
 const settings = ['Location', 'Share', 'Parameters', 'Logout'];
 
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  '& .MuiSwitch-switchBase': {
+    margin: 1,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(22px)',
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+    width: 32,
+    height: 32,
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        '#fff',
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+    borderRadius: 20 / 2,
+  },
+}));
+
+
+
+
+
+
+
+
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const [Language, setLanguage] = React.useState('en');
-
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
-    
   };
 
+  //I18n
+  const [Language, setLanguage] = React.useState('en');
   const handleChange = (event) => {
     setLanguage(event.target.value);
     i18next.changeLanguage(event.target.value)
   };
 
-  const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
+  // Dark/Light mode Gestion
+  const storedTheme = localStorage.getItem("theme");
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  };
+
+  const prefersDark =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const wantDark =
+    storedTheme === "dark" || (storedTheme === null && prefersDark);
+  if (wantDark) {
+    setDark();
+  }
+
+  const toggleTheme = () => {
+    if (localStorage.getItem("theme") === "light") {
+      setDark();
+    } else {
+      setLight();
+    }
+  };
 
   return (
     <AppBar position="static" 
@@ -134,24 +209,16 @@ const ResponsiveAppBar = () => {
           </Box>
 
           {/* Dark/Light Mode */}
-          <Box
-            sx={{
-              display: 'flex',
-              width: '100%',
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: 1,
-              p: 3,
-            }}
-          >
-            {theme.palette.mode} mode
-            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-              {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-            </IconButton>
+          <Box>
+            <FormControlLabel
+              control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+              label=""
+              onClick={toggleTheme} 
+            />
           </Box>
 
           {/* Languages list */}
-          <Box sx={{ minWidth: {xs: 50, md: 100}}}>         
+          <Box sx={{ minWidth: {xs: 100, md: 100}}}>         
             <FormControl fullWidth>
               <Select
                 value={Language}
@@ -159,7 +226,7 @@ const ResponsiveAppBar = () => {
                 variant= 'outlined'
                 sx={{bgcolor: 'white', mr: 2,  borderRadius: 2,}}
               >
-                <MenuItem value={'en'}>En</MenuItem>
+                <MenuItem value={'en'}>EN</MenuItem>
                 <MenuItem value={'fr'}>FR</MenuItem>
               </Select>
             </FormControl>
@@ -202,3 +269,4 @@ const ResponsiveAppBar = () => {
 };
 
 export default ResponsiveAppBar;
+
